94c705942e5657d1696e661d63da0af7e393d51a
ibrahim.baykal <ibrahim.baykal@afad.gov.tr>
2018-02-15 17:42:03 +0300

    first commit

---
+++ src/components/search_bar.js
@@ -0,0 +1,30 @@
+import React,{Component} from "react";
+
+class SearchBar extends Component{
+
+    constructor(props){
+        super(props);
+
+        this.state = {term : ""};
+    }
+
+    render(){
+        return(
+            <div>
+                <input onChange={event => this.setState({ term : event.target.value})}/>
+            </div>
+        );
+    }
+
+    /*
+     render(){
+     return <input onChange={this.onInputChange}/>;
+     }
+
+     onInputChange(event){
+     console.log(event.target.value);
+     }
+     */
+}
+
+export default SearchBar;
