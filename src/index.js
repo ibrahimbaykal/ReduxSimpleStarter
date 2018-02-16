a9b6d701335aa68fc9f31b08bc723244106ac55a
ibrahim.baykal <ibrahim.baykal@afad.gov.tr>
2018-02-16 10:41:10 +0300

    video selection

--- src/index.js
+++ src/index.js
@@ -15,14 +15,20 @@ class App extends Component {
     constructor(props){
         super(props);
 
-        this.state = { videos:[] };
+        this.state = {
+            videos:[],
+            selectedVideo : null
+        };
 
         YTSearch(
             {
                 key : API_KEY,term:"Beşiktaş"
             },
             (videos) => {
-                this.setState({videos})
+                this.setState({
+                    videos :videos,
+                    selectedVideo : videos[0]
+                });
             }
         );
     }
@@ -31,8 +37,10 @@ class App extends Component {
         return(
             <div>
                 <SearchBar />
-                <VideoDetail video ={this.state.videos[0]} />
-                <VideoList videos = {this.state.videos} />
+                <VideoDetail video ={this.state.selectedVideo} />
+                <VideoList
+                    onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
+                    videos = {this.state.videos} />
             </div>
         );
     }
